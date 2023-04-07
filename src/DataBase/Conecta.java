053a353b1aea8af728e682da97e72157eec090bf
ArianaMesquita <arianamesquita85@gmail.com>
2023-04-07 17:06:47 -0300

    Conexão banco de dados e jar

---
+++ src/DataBase/Conecta.java
@@ -0,0 +1,32 @@
+package DataBase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class Conecta {
+
+	public static void main(String[] args) {
+		
+		
+		final String DRIVER = "com.mysql.cj.jdbc.Driver";
+		final String URL = "jdbc:mysql://127.0.0.1:3306/?user=administrator";
+		
+
+		try {
+			Class.forName(DRIVER);
+			Connection connection = DriverManager.getConnection(URL, "administrator", "@Ari1#Ped2");
+			JOptionPane.showMessageDialog(null, "Conexão realizada com sucesso");
+			connection.close();	
+		} catch (ClassNotFoundException erro) {
+			JOptionPane.showMessageDialog(null, "Driver não encontrado!\n"
+					+ erro.toString());
+		} catch (SQLException erro) {
+			JOptionPane.showMessageDialog(null, "Problemas na conexão com a fonte de dados\n"
+					+ erro.toString());
+		}
+		
+	}
+}
