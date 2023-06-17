7c9777deccd0a8c13ffe3c52b27d40a64ea513c3
ArianaMesquita <arianamesquita85@gmail.com>
2023-06-17 13:53:49 -0300

    ajustes

--- src/src/DataBase/BancoAgenciaDAO/BancoAgenciaDAO.java
+++ src/src/DataBase/BancoAgenciaDAO/BancoAgenciaDAO.java
@@ -1,5 +1,56 @@
 package DataBase.BancoAgenciaDAO;
 
-public class BancoAgenciaDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import DataBase.Conexao;
+import DataBase.Factory;
+import DataBase.InterfaceDAO;
+import model.Agencia;
+
+public class BancoAgenciaDAO implements InterfaceDAO<Agencia>{
+
+    @Override
+    public void criar(Agencia obT) {
+
+        String sql = "insert into bancoAgencia(nomeBanco, codigoBanco, numeroAgencia, enderecoAgencia, telefoneAgencia)" +
+                        "values (?,?,?,?,?)";
+
+        Conexao conexao;
+        PreparedStatement pstm;
+
+        try{
+            conexao = Factory.creatConnectionToMySQL();
+            conexao.Conecta();
+            pstm = conexao.getConnection().prepareStatement(sql);
+
+            pstm.setString(1, sql);
+        }
+
+    }
+
+    @Override
+    public void ler(int id) {
+
+    }
+
+    @Override
+    public void atualizar(Agencia objeto) {
+
+    }
+
+    @Override
+    public void deletar(int id) {
+
+    }
+
+    @Override
+    public List<Agencia> listarTodos() {
+
+    }
     
+
+
+
 }
--- src/src/DataBase/ClienteDAO/PessoaFisicaDAO.java
+++ src/src/DataBase/ClienteDAO/PessoaFisicaDAO.java
@@ -12,31 +12,8 @@ public class PessoaFisicaDAO  implements InterfaceDAO<PessoaFisica> {
 
     @Override
     public void criar(PessoaFisica obT) {
-        Conexao conexao = null;
-        String query =  "INSERT INTO cliente (id, nome, endereco, telefone, cnpj, funcionarioResponsavel, rendaAtual, cpf) "+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-
-        try{
-            conexao = Factory.creatConnectionToMySQL();
-            conexao.Conecta();
-            conexao.setPstmt(conexao.getConnection().prepareStatement(query));
-            conexao.getPstmt().setInt(1, 2);
-            conexao.getPstmt().setString(2, getNome());
-            conexao.getPstmt().setString(3,getEndereco());
-            conexao.getPstmt().setString(4, getTelefone());
-            conexao.getPstmt().setString(5, null);
-            conexao.getPstmt().setString(6, null);
-            conexao.getPstmt().setDouble(7, getRendaAtual());
-            conexao.getPstmt().setString(8, getCpf());
-
-            conexao.getPstmt().executeUpdate();
-            conexao.getPstmt().close();
-
-            conexao.Desconecta();
-            System.out.println("Dados inseridos com sucesso na tabela pessoa fisica.");
-
-        } catch (SQLException e) {
-            System.out.println("Erro ao inserir dados na tabela pessoa: " + e.getMessage());
-        }
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'criar'");
     }
 
     @Override
@@ -62,6 +39,7 @@ public class PessoaFisicaDAO  implements InterfaceDAO<PessoaFisica> {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'listarTodos'");
     }
+
 
 
 
