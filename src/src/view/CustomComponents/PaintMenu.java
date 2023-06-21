1a0a332c56e0dec0c2f213339c8cb230dc6a73b4
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2023-06-21 10:10:22 -0300

    update

--- src/src/view/CustomComponents/PaintMenu.java
+++ src/src/view/CustomComponents/PaintMenu.java
@@ -1,12 +1,8 @@
 package view.CustomComponents;
 
-import java.awt.BorderLayout;
 import java.awt.Graphics;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-
-import view.viewAdds.PagamentosField;
 
 public class PaintMenu extends JPanel{
     private int rounded = 10;
@@ -26,25 +22,6 @@ public class PaintMenu extends JPanel{
         this.rounded = rounded;
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        JPanel paintMenu = new JPanel();
-        frame.setLayout(null);
-        //paintMenu.setBackground(Color.red);
-        //paintMenu.setLayout(new BorderLayout());
-        PagamentosField pagamentosField = new PagamentosField();
-        pagamentosField.setSize(200, 200);
-        paintMenu.setBounds(100, 100, 800, 800);
-        paintMenu.setOpaque(false);
-        PaintMenu painel = new PaintMenu();
-        painel.setLayout(new BorderLayout());
-        painel.setBounds(500, 200, 300, 300);
-        painel.setSize(400, 400);
-    
-        painel.add(paintMenu, BorderLayout.CENTER);
-        frame.add(painel);
-        paintMenu.add(pagamentosField, BorderLayout.WEST);
-        frame.setVisible(true);
-    }
+
 }
 
