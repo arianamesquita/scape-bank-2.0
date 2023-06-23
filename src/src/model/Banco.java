f8cd3f09d082f9f760ecf7332b12015a85b82f60
anna ruth <anna.ruthsg@gmail.com>
2023-06-23 10:47:33 -0300

    documentation  of  class Banco.java

--- src/src/model/Banco.java
+++ src/src/model/Banco.java
@@ -1,5 +1,9 @@
 package model;
 
+/**
+ * @author ArianaMesquita
+ * classe que cria banco;
+ */
 public class Banco {
 
     
@@ -7,22 +11,46 @@ public class Banco {
     public int codigo;
 
 
-    public Banco () {  }    
+    public Banco () {  }
+
+    /**
+     *
+     * @param nome = parametro do construtor da classe banco que recebe o nome do banco;
+     * @param codigo = parametro do construtor da classe banco que recebe o codigo do banco;
+     */
     public Banco(String nome, int codigo) {
         this.nome = nome;
         this.codigo = codigo;
     }
 
-
+    /**
+     *
+     * @return methodo da class banco que retorna nome do banco;
+     */
     public String getNome() {
         return nome;
     }
+
+    /**
+     * cadastra nome do banco;
+     * @param nome = parametro do metodo setNome que recebe nome do banco;
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    /**
+     *
+     * @return metodo da classe banco que retorna codigo;
+     */
     public int getCodigo() {
         return codigo;
     }
+
+    /**
+     * cadastra codigo do banco;
+     * @param codigo = parametro do metodo setCodigo que recebe codigo do banco;
+     */
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
