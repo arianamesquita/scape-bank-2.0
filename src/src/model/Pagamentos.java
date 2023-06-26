48950ebdbbebbc89bef6b7a650e3f5a050653a5f
Anna Ruth <anna.ruthsg@gmail.com>
2023-06-25 21:47:32 -0300

    documentação da classe Pagamentos

--- src/src/model/Pagamentos.java
+++ src/src/model/Pagamentos.java
@@ -1,17 +1,34 @@
 package model;
 
+/**
+ * @author ArianaMesquita
+ */
 public class Pagamentos {
 
     public String chavePix;
 
     public Pagamentos ( ) {   }
+
+    /**
+     *
+     * @param chavePix = parametro do construtor da classe Pagamentos que recebe uma chave pix;
+     */
     public Pagamentos(String chavePix) {
         this.chavePix = chavePix;
     }
 
+    /**
+     *
+     * @return método da classe Pagamentos que retorna chave pix;
+     */
     public String getChavePix() {
         return chavePix;
     }
+
+    /**
+     * cadastra chave pix;
+     * @param chavePix = parametro do método setChavePix que recebe uma chave pix;
+     */
     public void setChavePix(String chavePix) {
         this.chavePix = chavePix;
     }
